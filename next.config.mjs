580335ed5b9f423ef75aab97f3890d2127eb8e20
Jake Lee <freejak5520@gmail.com>
2024-05-23 22:58:24 +0900

    프로덕션 환경 data-test 로 시작하는 attribute 제거 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    reactRemoveProperties:
+      process.env.NODE_ENV === "production"
+        ? { properties: ["^data-test.*$"] }
+        : false,
+  },
+};
 
 export default nextConfig;
